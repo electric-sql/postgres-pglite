1adcf03727206cad8a7c568b8f84737017b70e9c
tudor <tudor@swisstch.com>
2025-01-16 18:22:52 +0100

    optimize build all

--- pglite/cibuild/build-all.sh
+++ pglite/cibuild/build-all.sh
@@ -2,17 +2,4 @@
 echo "======== build-all.sh : $(pwd)             =========="
 echo "======== Building all PGlite prerequisites =========="
 
-# move copy of patches into dir
-# not mounting them directly as lots of files are created
-# cp -rf /opt/patches ./patches
-
-apt update && apt install -y build-essential libreadline-dev zlib1g-dev bison flex git
-export FLEX=`which flex`
-
-. ./cibuild.sh
-
-. ./cibuild.sh contrib
-. ./cibuild.sh extra
-. ./cibuild.sh node
-. ./cibuild.sh linkweb
-. ./cibuild.sh postgres-pglite-dist
+. ./cibuild.sh contrib extra node linkweb postgres-pglite-dist
