9e0fe66ccdc276c34a74b06ccacc45d8199316b6
tudor <tudor@swisstch.com>
2025-02-04 15:16:34 +0100

    2 less bash scripts

--- pglite/cibuild/build-with-docker.sh
+++ pglite/cibuild/build-with-docker.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-echo "======== build-with-dockerl.sh : $(pwd)                 =========="
-echo "======== Building all PGlite prerequisites using Docker =========="
+echo "======== build-with-docker.sh : $(pwd)                 =========="
 
-trap 'echo caught interrupt and exiting;' INT
+# these are all the elements that can be build as part of this project
+ALL="contrib extra node linkweb postgres-pglite-dist"
+
+# this is what we will actually build
+WHAT=${*:-$ALL}
+
+echo "======== Building PGlite prerequisites ${WHAT} using Docker =========="
+
+# trap 'echo caught interrupt and exiting;' INT
 
 source ./pglite/.buildconfig
 
@@ -12,17 +19,14 @@ if [[ -z "$SDK_VERSION" || -z "$PG_VERSION" ]]; then
   exit 1
 fi
 
+# we are using a custom emsdk to build pglite wasm
+# this is available as a docker image under electricsql/pglite-builder
 IMG_NAME="electricsql/pglite-builder"
 IMG_TAG="${PG_VERSION}_${SDK_VERSION}"
-
-SDK_ARCHIVE="${SDK_ARCHIVE:-python3.13-wasm-sdk-Ubuntu-22.04.tar.lz4}"
-WASI_SDK_ARCHIVE="${WASI_SDK_ARCHIVE:-python3.13-wasi-sdk-Ubuntu-22.04.tar.lz4}"
 
 docker run \
   --rm \
   -e OBJDUMP=${OBJDUMP:-true} \
-  -e SDK_ARCHIVE \
-  -e WASI_SDK_ARCHIVE \
   -e PGSRC=/workspace/postgres-src \
   -e POSTGRES_PGLITE_OUT=/workspace/dist \
   -v ./pglite/cibuild.sh:/workspace/cibuild.sh:rw \
@@ -34,4 +38,4 @@ docker run \
   -v .:/workspace/postgres-src \
   -v ./pglite/dist:/workspace/dist \
   $IMG_NAME:$IMG_TAG \
-  bash ./cibuild/build-all.sh
+  bash /workspace/cibuild.sh $WHAT
