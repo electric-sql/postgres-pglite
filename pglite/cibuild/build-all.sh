8e4580c1b897de9d2617869d14380158f9c4cb4d
Tudor Zaharia <37596026+copiltembel@users.noreply.github.com>
2025-02-10 10:38:59 +0100

    2 less bash scripts (#15)
    Co-authored-by: tudor <tudor@swisstch.com>

--- pglite/cibuild/build-all.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "======== build-all.sh : $(pwd)             =========="
-echo "======== Building all PGlite prerequisites =========="
-
-. ./cibuild.sh contrib extra node linkweb postgres-pglite-dist
