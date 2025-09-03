e27aa640f8c51c98e65b02af8845280e03e1b721
tudor <tudor.zaharia@gmail.com>
2025-09-03 10:32:40 +0200

    cleanup

--- build-pglite.sh
+++ build-pglite.sh
@@ -18,8 +18,6 @@ else
 fi
 
 # Step 1: configure the project
-# echo "LDFLAGS: $LDFLAGS"
-# echo "CFLAGS: $CFLAGS"
 LDFLAGS="-sWASM_BIGINT -sUSE_PTHREADS=0" CFLAGS="${PGLITE_CFLAGS} -sWASM_BIGINT -fpic -sENVIRONMENT=node,web,worker -sSUPPORT_LONGJMP=emscripten -DPYDK=1 -DCMA_MB=12 -Wno-declaration-after-statement -Wno-macro-redefined -Wno-unused-function -Wno-missing-prototypes -Wno-incompatible-pointer-types" emconfigure ./configure ac_cv_exeext=.cjs --disable-spinlocks --disable-largefile --without-llvm  --without-pam --disable-largefile --with-openssl=no --without-readline --without-icu --with-includes=$INSTALL_PREFIX/include:$INSTALL_PREFIX/include/libxml2 --with-libraries=$INSTALL_PREFIX/lib --with-uuid=ossp --with-zlib --with-libxml --with-libxslt --with-template=emscripten --prefix=$INSTALL_FOLDER || { echo 'error: emconfigure failed' ; exit 11; }
 
 # Step 2: make and install all except pglite
