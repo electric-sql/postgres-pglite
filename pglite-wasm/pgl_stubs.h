174398816c4f71b7649c6ed583b3a333b22e0197
tudor <tudor.zaharia@gmail.com>
2025-09-24 13:46:50 +0200

    readd initdb requirements

--- pglite-wasm/pgl_stubs.h
+++ pglite-wasm/pgl_stubs.h
@@ -29,6 +29,41 @@ PostgresMain(const char *dbname, const char *username) {
 
 
 // embedded initdb requirements
+void
+get_restricted_token(void) {
+    // stub
+}
+
+void *
+pg_malloc(size_t size) {
+	return malloc(size);
+}
+
+void *
+pg_malloc_extended(size_t size, int flags) {
+    return malloc(size);
+}
+
+void *
+pg_realloc(void *ptr, size_t size) {
+    return realloc(ptr, size);
+}
+
+char *
+pg_strdup(const char *in) {
+	char	   *tmp;
+
+	if (!in) {
+    	fprintf(stderr, _("cannot duplicate null pointer (internal error)\n"));
+		exit(EXIT_FAILURE);
+	}
+	tmp = strdup(in);
+	if (!tmp) {
+		fprintf(stderr, _("out of memory\n"));
+		exit(EXIT_FAILURE);
+	}
+	return tmp;
+}
 
 char *
 simple_prompt(const char *prompt, bool echo) {
