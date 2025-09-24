bd5acf4a2908bb6123e7d547a19786485b64be4c
tudor <tudor.zaharia@gmail.com>
2025-09-24 11:18:28 +0200

    cleanup

--- clean-pglite.sh
+++ clean-pglite.sh
@@ -4,4 +4,4 @@ emmake make PORTNAME=emscripten -C src/backend uninstall; emmake make PORTNAME=e
 emmake make PORTNAME=emscripten -C pglite/ clean; emmake make PORTNAME=emscripten -C pglite/ uninstall;
 emmake make PORTNAME=emscripten -C contrib/ clean; emmake make PORTNAME=emscripten -C contrib/ uninstall; 
 emmake make PORTNAME=emscripten -C pglite clean; emmake make PORTNAME=emscripten -C pglite uninstall;
-emmake make PORTNAME=emscripten clean; emmake make PORTNAME=emscripten uninstall
+emmake make PORTNAME=emscripten clean; emmake make PORTNAME=emscripten uninstall;
