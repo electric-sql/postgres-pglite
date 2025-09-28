4ba6f2c86b5557eca6d6ce18e76f5fdb3176e3ac
tudor <tudor.zaharia@gmail.com>
2025-09-28 18:32:37 +0200

    changes

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -8,6 +8,6 @@ docker run $@ \
   --workdir=${DOCKER_WORKSPACE} \
   -v .:${DOCKER_WORKSPACE}:rw \
   -v ./dist:/install/pglite:rw \
-  electricsql/pglite-builder:3.1.74_1 \
+  electricsql/pglite-builder:pglite-postgis \
   ./build-pglite.sh
   
