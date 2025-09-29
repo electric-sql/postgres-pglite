1fb2e61c34a6ce8703cc12ca07a212cefecc0359
tudor <tudor.zaharia@gmail.com>
2025-09-29 10:30:55 +0200

    link libstdc++; other changes to enable building postgis

--- clean-pglite.sh
+++ clean-pglite.sh
@@ -3,4 +3,4 @@
 emmake make -C src/backend uninstall; emmake make -C src/backend clean;
 emmake make -C pglite/ clean; emmake make -C pglite/ uninstall;
 emmake make -C contrib/ clean; emmake make -C contrib/ uninstall; emmake make -C pglite clean; emmake make -C pglite uninstall;
-emmake make clean; emmake make uninstal
+emmake make clean; emmake make uninstall
---
+++ pglite/build-postgis.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+cd postgis
+./autogen.sh
+# LDFLAGS="-L/install/libs/lib" emconfigure ./configure --with-pic --with-xml2config=/install/libs/bin/xml2-config --with-geosconfig=/install/libs/bin/geos-config --with-projdir=/install/libs/ --with-jsondir=/install/libs/ --without-protobuf --with-gdalconfig=/install/libs/bin/gdal-config
+# emconfigure ./configure --with-pic --with-geosconfig=/install/libs/bin/geos-config --with-projdir=/install/libs/ --with-xml2config=/install/libs/bin/xml2-config --with-jsondir=/install/libs/ --without-protobuf --without-raster --enable-static=no --enable-shared=yes
+PROJ_VERSION=9.7.0 LDFLAGS="-L/install/libs/lib" emconfigure ./configure --with-pic --without-protobuf --without-raster --enable-static=no --enable-shared=yes --with-xml2config=/install/libs/bin/xml2-config --with-geosconfig=/install/libs/bin/geos-config --with-projdir=/install/libs/ --host=wasm32-unknown-none
+emmake make LDFLAGS_SL="-sWASM_BIGINT -L/install/libs/lib -lpgport -lpgcommon -sSIDE_MODULE=1 -fexceptions" CFLAGS_SL="-fexceptions -sWASM_BIGINT" CXXFLAGS="-fexceptions -sWASM_BIGINT" -j
+# emmake make PG_LDFLAGS="-L/install/libs/lib -lpgport -lpgcommon -sSIDE_MODULE=1" -j
+
+cd ..
