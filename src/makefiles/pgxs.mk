b67a2604402eebd8596342f3f855fa5aa94dccbd
tudor <tudor.zaharia@gmail.com>
2025-09-30 08:35:23 +0200

    do not links libstdc++ to main module. fixes in pgxs when getting imported/exported functions.

--- src/makefiles/pgxs.mk
+++ src/makefiles/pgxs.mk
@@ -250,9 +250,9 @@ ifeq ($(with_llvm), yes)
 	$(foreach mod, $(MODULES), $(call install_llvm_module,$(mod),$(mod).bc))
 endif # with_llvm
 ifeq ($(PORTNAME), emscripten)
-	$(LLVM_NM) -u *.o    | awk '{print $$2}'              | sed '/^$$/d' | sort -u > $(MODULES).undef.txt
-	$(LLVM_NM)    *.o    | awk '$$2 ~ /^[TDB]$$/ {print $$3}' | sed '/^$$/d' | sort -u > $(MODULES).defs.txt
-	comm -23 $(MODULES).undef.txt $(MODULES).defs.txt > '$(emscripten_extension_imports_dir)/$(MODULES).imports'
+	$(LLVM_NM) -u *.o    | awk '{print $$2}'              | sed '/^$$/d' | sort -u > '$(MODULES).undef.txt'
+	$(LLVM_NM)    *.o    | awk '$$2 ~ /^[TDB]$$/ {print $$3}' | sed '/^$$/d' | sort -u > '$(MODULES).defs.txt'
+	comm -23 '$(MODULES).undef.txt' '$(MODULES).defs.txt' > '$(emscripten_extension_imports_dir)/$(MODULES).imports'
 endif # PORTNAME=emscripten
 endif # MODULES
 ifdef DOCS
@@ -277,9 +277,9 @@ ifeq ($(with_llvm), yes)
 	$(call install_llvm_module,$(MODULE_big),$(OBJS))
 endif # with_llvm
 ifeq ($(PORTNAME), emscripten)
-	$(LLVM_NM) -u $(OBJS)    | awk '{print $$2}'              | sed '/^$$/d' | sort -u > $(MODULE_big).undef.txt
-	$(LLVM_NM)    $(OBJS)    | awk '$$2 ~ /^[TDB]$$/ {print $$3}' | sed '/^$$/d' | sort -u > $(MODULE_big).defs.txt
-	comm -23 $(MODULE_big).undef.txt $(MODULE_big).defs.txt > '$(emscripten_extension_imports_dir)/$(MODULE_big).imports'
+	$(LLVM_NM) -u $(OBJS)    | awk '{print $$2}'              | sed '/^$$/d' | sort -u > '$(MODULE_big).undef.txt'
+	$(LLVM_NM)    $(OBJS)    | awk '$$2 ~ /^[TDB]$$/ {print $$3}' | sed '/^$$/d' | sort -u > '$(MODULE_big).defs.txt'
+	comm -23 '$(MODULE_big).undef.txt' '$(MODULE_big).defs.txt' > '$(emscripten_extension_imports_dir)/$(MODULE_big).imports'
 endif # PORTNAME=emscripten
 
 install: install-lib
