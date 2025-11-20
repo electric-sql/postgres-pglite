ea595fc1db479fb7340e03112b87f6e57a139170
tudor <tudor@swisstch.com>
2025-11-20 17:06:14 +0100

    system call

--- pglite/src/include/pglite-system.h
+++ pglite/src/include/pglite-system.h
@@ -1,7 +1,19 @@
 #if defined(__PGLITE__)
 
+#if defined(__EMSCRIPTEN__)
+#include <emscripten/emscripten.h>
+#else
+#define EMSCRIPTEN_KEEPALIVE
+// TODO: an include for libpglite
+#endif
+
 typedef ssize_t (*pglite_system_t)(void *command);
 pglite_system_t pglite_system = NULL;
+
+void EMSCRIPTEN_KEEPALIVE
+pgl_set_system_fn(pglite_system_t system_fn) {
+    pglite_system = system_fn;
+}
 
 int system(const char *command) {
     if (pglite_system) {
