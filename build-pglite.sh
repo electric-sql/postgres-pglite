16ea22ce27216655c644589628f48ec6bd760395
tudor <tdrz@zaharia.ch>
2025-12-16 15:19:26 +0100

    undo changes to build_pglite.sh

--- build-pglite.sh
+++ build-pglite.sh
@@ -9,17 +9,17 @@ INSTALL_FOLDER=${INSTALL_FOLDER:-"/install/pglite"}
 
 # build with optimizations by default aka release
 # PGLITE_CFLAGS="-sDYLINK_DEBUG=2 -g -gsource-map --no-wasm-opt -Wbad-function-cast -Wcast-function-type"
-PGLITE_CFLAGS="--emit-symbol-map -O2"
-# if [ "$DEBUG" = true ]
-# then
-#     echo "pglite: building debug version."
-#     PGLITE_CFLAGS="$PGLITE_CFLAGS -g -gsource-map --no-wasm-opt"
-# else
-#     echo "pglite: building release version."
-#     PGLITE_CFLAGS="$PGLITE_CFLAGS -O2"
-#     # we shouldn't need to do this, but there's a bug somewhere that prevents a successful build if this is set
-#     unset DEBUG
-# fi
+PGLITE_CFLAGS=""
+if [ "$DEBUG" = true ]
+then
+    echo "pglite: building debug version."
+    PGLITE_CFLAGS="$PGLITE_CFLAGS -g -gsource-map --no-wasm-opt --emit-symbol-map"
+else
+    echo "pglite: building release version."
+    PGLITE_CFLAGS="$PGLITE_CFLAGS -O2"
+    # we shouldn't need to do this, but there's a bug somewhere that prevents a successful build if this is set
+    unset DEBUG
+fi
 
 echo "pglite: PGLITE_CFLAGS=$PGLITE_CFLAGS"
 
