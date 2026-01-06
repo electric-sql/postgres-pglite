b798740aed34a3acac1b706a1459b1a35cff203e
tudor <tdrz@zaharia.ch>
2026-01-06 16:00:49 +0100

    move postgis dist to own folder

--- build-pglite.sh
+++ build-pglite.sh
@@ -76,7 +76,8 @@ PATH=$PATH:$INSTALL_FOLDER/bin
 # postgis - special case
 cd ./pglite/ && ./build-postgis.sh && cd ../
 emmake make OPTFLAGS="" PORTNAME=emscripten -j -C pglite || { echo 'error: emmake make OPTFLAGS="" PORTNAME=emscripten -j -C pglite' ; exit 41; }
-emmake make OPTFLAGS="" PORTNAME=emscripten -C pglite/ dist || { echo 'error: make OPTFLAGS="" PORTNAME=emscripten -C pglite/ dist ' ; exit 42; }
+emmake make OPTFLAGS="" PORTNAME=emscripten -C pglite/ dist || { echo 'error: emmake make OPTFLAGS="" PORTNAME=emscripten -C pglite/ postgis.tar.gz ' ; exit 42; }
+emmake make OPTFLAGS="" PORTNAME=emscripten -C pglite/ dist-postgis || { echo 'emmake make OPTFLAGS="" PORTNAME=emscripten -C pglite/ dist-postgis' ; exit 43; }
 PATH=$SAVE_PATH
 
 # Step 5: make and install pglite
