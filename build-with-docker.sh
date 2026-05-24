f64fddc0a46e13a1a15e7745cc62c8861a9f6d07
Tudor Zaharia <tdrz@zaharia.ch>
2026-05-24 10:00:25 +0200

    use PGLITE_VERSION in version() output

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -1,10 +1,13 @@
 # although we could use any path inside docker, using the same path as on the host
 # allows the DWARF info (when building in DEBUG) to contain the correct file paths
 DOCKER_WORKSPACE=$(pwd)
+echo "PGLITE_VERSION=${PGLITE_VERSION}"
+echo "DEBUG=${DEBUG}"
 
 docker run $@ \
   --rm \
   -e DEBUG=${DEBUG:-false} \
+  -e PGLITE_VERSION=${PGLITE_VERSION} \
   --workdir=${DOCKER_WORKSPACE} \
   -v .:${DOCKER_WORKSPACE}:rw \
   -v ./dist:/pglite:rw \
