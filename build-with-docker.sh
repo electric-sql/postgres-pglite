9b345dd6ec3c56ad286a061d80a7cfff2dc781ee
Tudor Zaharia <37596026+tdrz@users.noreply.github.com>
2026-05-25 13:09:48 +0200

    Tdrz/be new libicu (#75)
    * libicu support

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -11,5 +11,5 @@ docker run $@ \
   --workdir=${DOCKER_WORKSPACE} \
   -v .:${DOCKER_WORKSPACE}:rw \
   -v ./dist:/pglite:rw \
-  electricsql/pglite-builder:3.1.74-5-postgis \
+  electricsql/pglite-builder:3.1.74-5-postgis-libicu-min \
   ./build-pglite.sh
