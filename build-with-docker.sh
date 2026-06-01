956441b31494dc8a1e6606200119b944f137ee7e
Tudor Zaharia <37596026+tdrz@users.noreply.github.com>
2026-06-01 12:49:45 +0200

    Add GDAL support for PostGIS (#76)
    * GDAL support for postgis

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -11,5 +11,5 @@ docker run $@ \
   --workdir=${DOCKER_WORKSPACE} \
   -v .:${DOCKER_WORKSPACE}:rw \
   -v ./dist:/pglite:rw \
-  electricsql/pglite-builder:3.1.74-6-postgis-libicu-min \
+  electricsql/pglite-builder:3.1.74-7 \
   ./build-pglite.sh
